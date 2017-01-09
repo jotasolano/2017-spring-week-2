23ac7383dbc5e6333faea73c5da71e5e2e81232d
Siqi Zhu <szhu@sasaki.local>
2017-01-09 13:05:38 -0500

    First commit on master

---
+++ public/script.js
@@ -0,0 +1,61 @@
+//Set up a drawing environment
+var m = {t:50,r:50,b:50,l:50},
+	w = document.getElementById('plot1').clientWidth - m.l - m.r,
+	h = document.getElementById('plot1').clientHeight - m.t - m.b;
+var plots = d3.selectAll('.plot')
+	.append('svg')
+	.attr('width', w + m.l + m.r)
+	.attr('height', h + m.t + m.b)
+	.append('g')
+	.attr('class','canvas')
+	.attr('transform','translate('+m.l+','+m.t+')');
+var plot1 = plots.filter(function(d,i){ return i===0;});
+
+d3.queue()
+	.defer(d3.csv,'./data/hubway_trips_reduced.csv',parseTrips)
+	.defer(d3.csv,'./data/hubway_stations.csv',parseStations)
+	.await(dataLoaded);
+
+function dataLoaded(err,trips,stations){
+	console.log(trips);
+	console.log(stations);
+}
+
+function parseTrips(d){
+	return {
+		bike_nr:d.bike_nr,
+		duration:+d.duration,
+		startStn:d.strt_statn,
+		startTime:parseTime(d.start_date),
+		endStn:d.end_statn,
+		endTime:parseTime(d.end_date),
+		userType:d.subsc_type,
+		userGender:d.gender?d.gender:undefined,
+		userBirthdate:d.birth_date?+d.birth_date:undefined
+	}
+}
+
+function parseStations(d){
+	return {
+		id:d.id,
+		lngLat:[+d.lng,+d.lat],
+		city:d.municipal,
+		name:d.station,
+		status:d.status,
+		terminal:d.terminal
+	}
+}
+
+function parseTime(timeStr){
+	var time = timeStr.split(' ')[1].split(':'),
+		hour = +time[0],
+		min = +time[1],
+		sec = +time[2];
+
+	var	date = timeStr.split(' ')[0].split('/'),
+		year = date[2],
+		month = date[0],
+		day = date[1];
+
+	return new Date(year,month-1,day,hour,min,sec);
+}
